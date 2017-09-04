cdc5e1c1b96d0f8868b976084f661543071a58e7
Jhun24 <jjanghuni102@naver.com>
2017-09-04 09:08:51 +0900

    Setting project & add user Schema

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var mongoose = require('mongoose');
+var randomstring = require('randomstring');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html')
+app.set('views', 'views')
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +24,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+mongoose.connect('mongodb://localhost:27017/no') ;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    console.log("Mongo DB ON");
+});
+
+var user = mongoose.Schema({
+    name:String,
+    id:String,
+    ps:String,
+    token:String,
+    age:String,
+    sex:String,
+    work:String
+});
+
+var userModel = mongoose.model('userModel',user);
+
+require('./routes/auth')(app,randomstring);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +60,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+/**
+ * Created by janghunlee on 2017. 9. 4..
+ */
+module.exports = auth;
+
+function auth(app){
+    "use strict";
+
+}
